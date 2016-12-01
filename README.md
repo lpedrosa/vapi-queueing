f2a6b8021158cf8a92c6142e2b9ece2a9182b1d3
lpedrosa <lpedrosa88@gmail.com>
2016-12-01 18:23:12 +0000

    added some reasoning and started implementing the queue

--- README.md
+++ README.md
@@ -12,3 +12,13 @@ For this we need:
 * nexmo application id
 * a private key for minting JWTs
 * a nexmo number that will be the entry point of our application
+
+# Reasoning
+
+Every time someone rings the nexmo LVN that points to this application, we direct that call to a conversation. On that conversation we play a simple holding tune using the `stream` action.
+
+At the same time we capture the `conversation_id` and `call_id` of the call and submit this to an internal application queue.
+
+When an agent is ready to take the call (currently represented as a scheduled task that polls the queue), it unqueues the call information.
+
+It then transfers that `call_id` into a conversation with the agent already waiting for the caller (currently represented as a named conversation, in which we play a tts message representing the agent).
--- app.py
+++ app.py
@@ -1,16 +1,38 @@
-from bottle import get, post, request, run
+from bottle import get, post, request, response, run
+from Queue import Queue
 
 import os
 
-
+# provided by the environment (e.g. Heroku)
 PORT = os.getenv('PORT', 5000)
+
+call_queue = Queue()
+
+
+def json_response(container):
+    response.content_type = 'application/json'
+    import json
+    return json.dumps(container)
 
 
 @get('/queue')
 def queue_call():
-    ncco = {'action': 'conversation',
-            'name': 'call-queue'}
-    return ncco
+    from_addr = request.query['from']
+    conversation = request.query['conversation_uuid']
+    call = request.query['uuid']
+
+    print("Received call [conversation_id:{}, call_uuid: {} from: {}]"
+            .format(conversation, call, from_addr))
+
+    # store the uuid in queue
+    call_queue.put(call)
+
+    # serve ncco
+    ncco = [{'action': 'stream',
+             'streamUrl': 'someUrl',
+             'loop': 0}]
+
+    return json_response(ncco)
 
 
 @post('/queue/events')
@@ -19,4 +41,4 @@ def ncco_events():
     print("Received event: {}".format(event))
 
 
-run(host='0.0.0.0', port=PORT, server='gunicorn', workers='2')
+run(host='0.0.0.0', port=PORT, server='gunicorn', workers='1')
